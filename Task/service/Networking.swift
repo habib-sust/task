790659fd82dcb2d3e9a240d7649485fd12ce1612
Habibur Rahamn <shopon2024@gmail.com>
2019-03-13 13:51:10 +0600

    Add HTTPNetworking struct & createRequest method

--- Task/service/Networking.swift
+++ Task/service/Networking.swift
@@ -13,4 +13,13 @@ protocol NetWorking {
     func get(from stringURL: String, completion: @escaping completionHandler)
 }
 
+struct HTTPNetworking {
+    //******* MARK: - Private Methods *******
+    private func createRequest(from url: URL) -> URLRequest{
+        var request = URLRequest(url: url)
+        request.cachePolicy = .useProtocolCachePolicy
+        return request
+    }
+    
 
+}
