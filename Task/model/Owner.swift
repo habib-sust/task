e68a2e4e68a41503ce49853759843465bd9ab733
Habibur Rahamn <shopon2024@gmail.com>
2019-03-14 10:35:05 +0600

    Create Owner model

---
+++ Task/model/Owner.swift
@@ -0,0 +1,26 @@
+//
+//  Owner.swift
+//  Task
+//
+//  Created by Habib on 14/3/19.
+//  Copyright © 2019 Habib. All rights reserved.
+//
+
+import Foundation
+struct Owner : Codable {
+    let ownerName : String?
+    let avatarURL : String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case ownerName = "login"
+        case avatarURL = "avatar_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        ownerName = try values.decodeIfPresent(String.self, forKey: .ownerName)
+        avatarURL = try values.decodeIfPresent(String.self, forKey: .avatarURL)
+    }
+    
+}
