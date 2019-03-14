1ba84ed82b01d1464384b7b88f287d5c65a21556
Habibur Rahamn <shopon2024@gmail.com>
2019-03-14 16:47:08 +0600

    Create NoteDelegate protocol

--- Task/presenter/NotePresenter.swift
+++ Task/presenter/NotePresenter.swift
@@ -9,4 +9,18 @@
 import UIKit
 import RealmSwift
 
+protocol NoteDelegate {
+    func noteAddSucceed()
+    func noteAddDidFailedWith(_ message: String)
+    func fetchNoteSucceddWith(_ note: Note)
+    func fetchNoteDidFailedWith(_ message: String)
+    
+}
 
+protocol NoteAdd {
+    func addNoteWith(userId id: Int, note: String)
+}
+
+class NotePresenter: NSObject {
+
+}
